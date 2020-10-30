6e2a55ae451feab742d9899d75193a4d05357345
Connor Rothschild <connorrothschild@gmail.com>
2020-10-29 20:38:57 -0500

    add new data to COVID posts

--- public/post/covid-posts/scripts/clean_text.R
+++ public/post/covid-posts/scripts/clean_text.R
@@ -1,182 +1,186 @@
-# textDF_data$data <- stringr::str_trim(string = textDF_data$data, side = "both")
-textDF_data$data <- str_squish(string = textDF_data$data)
+# text_df$data <- stringr::str_trim(string = text_df$data, side = "both")
+text_df$data <- str_squish(string = text_df$data)
 
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "ACAPS$", replacement = "Assessment Capacities Project (ACAPS)")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "Apple Mobility Data", replacement = "Apple Mobility")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^Apple.*$", replacement = "Apple Mobility")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^Assessment.*$", replacement = "Assessment Capacities Project (ACAPS)")
-textDF_data$data <-
+text_df$data <-
+  str_replace(text_df$data, pattern = "ACAPS$", replacement = "Assessment Capacities Project (ACAPS)")
+text_df$data <-
+  str_replace(text_df$data, pattern = "Apple Mobility Data", replacement = "Apple Mobility")
+text_df$data <-
+  str_replace(text_df$data, pattern = "^Apple.*$", replacement = "Apple Mobility")
+text_df$data <-
+  str_replace(text_df$data, pattern = "^Assessment.*$", replacement = "Assessment Capacities Project (ACAPS)")
+text_df$data <-
   ifelse(
-    str_detect(textDF_data$data, pattern = "Labor Statistics"),
+    str_detect(text_df$data, pattern = "Labor Statistics"),
     "U.S. Bureau of Labor Statistics",
-    textDF_data$data
+    text_df$data
   )
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "corona virus package", replacement = "coronavirus package")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^Covid Tracking.*$", replacement = "COVID Tracking Project")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "covidtracking.com", replacement = "COVID Tracking Project")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "COVID Tracking project", replacement = "COVID Tracking Project")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(text_df$data, pattern = "corona virus package", replacement = "coronavirus package")
+text_df$data <-
+  str_replace(text_df$data, pattern = "^Covid Tracking.*$", replacement = "COVID Tracking Project")
+text_df$data <-
+  str_replace(text_df$data, pattern = "covidtracking.com", replacement = "COVID Tracking Project")
+text_df$data <-
+  str_replace(text_df$data, pattern = "COVID Tracking project", replacement = "COVID Tracking Project")
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "COVID-19 package",
               replacement = "COVID-19 package")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "COVID-19R",
               replacement = "COVID-19 package")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "Centers",
               replacement = "Centre")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "data.gouv.fr/fr/datasets/
 ",
               replacement = "data.gouv.fr")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "gouv.fr",
               replacement = "data.gouv.fr")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^European Centre.*$", replacement = "European Centre for Disease Control (ECDC)")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "European Center for Disease Control", replacement = "European Centre for Disease Control (ECDC)")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^ECDC.*$", replacement = "European Centre for Disease Control (ECDC)")
-textDF_data$data <- str_replace(
-  textDF_data$data,
+text_df$data <-
+  str_replace(text_df$data, pattern = "^European Centre.*$", replacement = "European Centre for Disease Control (ECDC)")
+text_df$data <-
+  str_replace(text_df$data, pattern = "European Center for Disease Control", replacement = "European Centre for Disease Control (ECDC)")
+text_df$data <-
+  str_replace(text_df$data, pattern = "^ECDC.*$", replacement = "European Centre for Disease Control (ECDC)")
+text_df$data <- str_replace(
+  text_df$data,
   pattern = "
 European Centre for Disease Control (ECDC) (ECDC)",
   replacement = "European Centre for Disease Control (ECDC)"
 )
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "french site data.data.gouv.fr",
               replacement = "data.gouv.fr")
-textDF_data$data <-
-  str_remove(textDF_data$data, pattern = "Geocodes from ")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^Google Mob.*$", replacement = "Google Mobility")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "Google mobility", replacement = "Google Mobility")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = 'Google \\"COVID-19 Community Mobility Reports\\"', replacement = "Google Mobility")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^Hopkins.*$", replacement = "Johns Hopkins Univ.")
-textDF_data$data <-
-  str_remove(string = textDF_data$data, pattern = "https\\:")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^Human Mortality.*$", replacement = "Human Mortality Database")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "I n stituto", replacement = "Instituto")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^Japan National.*$", replacement = "Japan National Land Numeric Information")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "Johns Hopkins$", replacement = "Johns Hopkins Univ.")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "microsimulation", replacement = "Simulation")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^Mobility Trends.*$", replacement = "Apple Mobility")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^New York T.*$", replacement = "New York Times")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "Not sourced", replacement = "No data")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_remove(text_df$data, pattern = "Geocodes from ")
+text_df$data <-
+  str_replace(text_df$data, pattern = "^Google Mob.*$", replacement = "Google Mobility")
+text_df$data <-
+  str_replace(text_df$data, pattern = "Google mobility", replacement = "Google Mobility")
+text_df$data <-
+  str_replace(text_df$data, pattern = 'Google \\"COVID-19 Community Mobility Reports\\"', replacement = "Google Mobility")
+text_df$data <-
+  str_replace(text_df$data, pattern = "^Hopkins.*$", replacement = "Johns Hopkins Univ.")
+text_df$data <-
+  str_remove(string = text_df$data, pattern = "https\\:")
+text_df$data <-
+  str_replace(text_df$data, pattern = "^Human Mortality.*$", replacement = "Human Mortality Database")
+text_df$data <-
+  str_replace(text_df$data, pattern = "I n stituto", replacement = "Instituto")
+text_df$data <-
+  str_replace(text_df$data, pattern = "^Japan National.*$", replacement = "Japan National Land Numeric Information")
+text_df$data <-
+  str_replace(text_df$data, pattern = "Johns Hopkins$", replacement = "Johns Hopkins Univ.")
+text_df$data <-
+  str_replace(text_df$data, pattern = "microsimulation", replacement = "Simulation")
+text_df$data <-
+  str_replace(text_df$data, pattern = "^Mobility Trends.*$", replacement = "Apple Mobility")
+text_df$data <-
+  str_replace(text_df$data, pattern = "^New York T.*$", replacement = "New York Times")
+text_df$data <-
+  str_replace(text_df$data, pattern = "Not sourced", replacement = "No data")
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "Opentable",
               replacement = "OpenTable")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "our world in data",
               replacement = "Our World in Data")
-textDF_data$data <-
-  str_remove(string = textDF_data$data, pattern = "regression, log-linear")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "^The Guard.*$", replacement = "The Guardian (Australia)")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_remove(string = text_df$data, pattern = "regression, log-linear")
+text_df$data <-
+  str_replace(text_df$data, pattern = "^The Guard.*$", replacement = "The Guardian (Australia)")
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "the united states project",
               replacement = "United States Project")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "simulation", replacement = "Simulation")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "S imulation", replacement = "Simulation")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "T ennessee", replacement = "Tennessee")
-textDF_data$data <-
-  str_replace(textDF_data$data, pattern = "U nited S tates P roject", replacement = "United States Project")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(text_df$data, pattern = "simulation", replacement = "Simulation")
+text_df$data <-
+  str_replace(text_df$data, pattern = "S imulation", replacement = "Simulation")
+text_df$data <-
+  str_replace(text_df$data, pattern = "T ennessee", replacement = "Tennessee")
+text_df$data <-
+  str_replace(text_df$data, pattern = "U nited S tates P roject", replacement = "United States Project")
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "US Census",
               replacement = "U.S. Census Bureau")
-textDF_data$data <-
+text_df$data <-
   ifelse(
-    str_detect(textDF_data$data, pattern = "Census"),
+    str_detect(text_df$data, pattern = "Census"),
     "U.S. Census Bureau",
-    textDF_data$data
+    text_df$data
   )
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "Wikipedia: Case statistics\\)",
               replacement = "Wikipedia")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "Wikipeida",
               replacement = "Wikipedia")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "W ikipeida",
               replacement = "Wikipedia")
-textDF_data$data <-
-  ifelse(str_detect(textDF_data$data, pattern = "Wikipedia"),
+text_df$data <-
+  ifelse(str_detect(text_df$data, pattern = "Wikipedia"),
          "Wikipedia",
-         textDF_data$data)
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+         text_df$data)
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "W ikipedia",
               replacement = "Wikipedia")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "US Census Bureau",
               replacement = "U.S. Census Bureau")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "Worldbank",
               replacement = "World Bank")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "WorldBank",
               replacement = "World Bank")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "World o meters",
               replacement = "Worldometers")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "W orldometers",
               replacement = "Worldometers")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "^World Pop.*$",
               replacement = "World Population Review (WPR)")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "WorldPop",
               replacement = "World Population Review (WPR)")
-textDF_data$data <-
-  str_replace(string = textDF_data$data,
+text_df$data <-
+  str_replace(string = text_df$data,
               pattern = "www.insee.fr",
               replacement = "INSEE")
-textDF_data$data <-
-  str_remove(string = textDF_data$data, pattern = "www\\.")
-textDF_data$data <- 
-  str_remove_all(textDF_data$data, "�")
-textDF_data$data <- 
-  str_remove_all(textDF_data$data, "//")
+text_df$data <-
+  str_replace(string = text_df$data,
+              pattern = fixed("Johns Hopkins Univ."),
+              replacement = "Johns Hopkins University")
+text_df$data <-
+  str_remove(string = text_df$data, pattern = "www\\.")
+text_df$data <- 
+  str_remove_all(text_df$data, "�")
+text_df$data <- 
+  str_remove_all(text_df$data, "//")
