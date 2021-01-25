bda66895632de33a1c386345024409d8ba48d009
Connor Rothschild <connorrothschild@gmail.com>
2021-01-24 23:05:39 -0600

    ?

--- publish.sh
+++ publish.sh
@@ -13,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -24,5 +24,5 @@ hugo
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-#echo "Pushing to github"
-#git push --all
+echo "Pushing to github"
+git push --all
