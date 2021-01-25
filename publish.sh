73a8bce6d095ebab013d48403f8238bb7a952ab5
Connor Rothschild <connorrothschild@gmail.com>
2021-01-24 23:01:47 -0600

    .

--- publish.sh
+++ publish.sh
@@ -13,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public https://github.com/connorrothschild/connorrothschild.com.git/gh-pages
+git worktree add -B gh-pages public gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
